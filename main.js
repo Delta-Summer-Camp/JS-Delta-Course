715d897edb4ef1d6d34213c090398442908ce122
Dima <d.ablov@gmail.com>
2020-07-26 14:43:47 +0300

    New game....

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-document.addEventListener('keypress', logKey);
+document.addEventListener('keydown', processKey);
+const STEP = 10;
+const X_MAX = 770;
+const Y_MAX = 770;
+const bSquare = document.getElementById('blackSquare');
 
-function logKey(event) {
-    let strOut = '';
-    if(event.ctrlKey === true) strOut += 'Ctrl+';
-    if(event.shiftKey === true) strOut += 'Shift+';
-    if(event.altKey === true) strOut + 'Att+';
-    strOut += event.code;
-    document.getElementById('output').innerText = strOut;
+function processKey(ev) {
+    let key = ev.keyCode;
+
+    let x = bSquare.offsetLeft;
+    let y = bSquare.offsetTop;
+
+    switch (key) {
+        case 37: // left
+            x -= STEP;
+            break;
+        case 38: //up
+            y -= STEP;
+            break;
+        case 39: // right
+            x += STEP;
+            break;
+        case 40: // down
+            y += STEP;
+            break;
+        default:
+            return false;
+    }
+
+    bSquare.style.left = x + 'px';
+    bSquare.style.top = y + 'px';
+
+    return true;
 }
-
